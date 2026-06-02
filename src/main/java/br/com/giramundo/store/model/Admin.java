edfe7db3bc5d243de1a9dc380fdf7849e9bdd1d6
andreyferraz <andrey.developer@hotmail.com>
2026-06-02 02:29:12 -0300

    feat: refactor Admin class to implement Persistable and use Lombok annotations

--- src/main/java/br/com/giramundo/store/model/Admin.java
+++ src/main/java/br/com/giramundo/store/model/Admin.java
@@ -2,40 +2,40 @@ package br.com.giramundo.store.model;
 
 import java.util.UUID;
 
-public class Admin {
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table("admin")
+public class Admin implements Persistable<UUID>{
+
+    @Id
+    @Column("id")
     private UUID id;
+
+    @Column("username")
     private String username;
+    
+    @Column("password")
     private String password;
 
-    public Admin() {}
+    @Transient 
+    private boolean isNew = false;
 
-    public Admin(UUID id, String username, String password) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-    }
+    @Override
+    public UUID getId() { return id; }
 
-    public UUID getId() {
-        return id;
-    }
+    @Override
+    public boolean isNew() { return isNew; }
 
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
